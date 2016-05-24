4146276348287ae7d5424c8cfdbd7178e510029c
Kiryha <kiryha@gmail.com>
2016-05-24 11:20:14 +0300

    Update setupProject.py

--- DNA/3D/setupProject.py
+++ DNA/3D/setupProject.py
@@ -1,34 +1,31 @@
-# ANIMATION DNA 2.0 
-# FTrack EDITION
+# Animation DNA 2.0
+# NO FTRACK EDITION
 # Set-up current project PATHES and SETTINGS
-# FTrack PROJECT NAME should match PROJECT ROOT FOLDER NAME ( pirates  ==  P:/PIRATES/) 
 
-import os, ftrack
+import os
 
 # Get ROOT of project from this file ('P:/PIRATES/')
-rootProject = os.path.dirname(__file__).split('PREP\PIPELINE\DNA')[0].replace("\\", "/") 
+projectRoot = os.path.dirname(__file__).split('PREP\PIPELINE\DNA')[0].replace("\\", "/") 
 # Get project NAME (PIRATES)
-projectName = rootProject.split('/')[-2]
-print '<<  setupProject [ rootProject ]: {0} >>'.format(rootProject)
+projectName = projectRoot.split('/')[-2]
+codeProject = projectName.upper()
+print '<<  setupProject [ projectRoot ]: {0} >>'.format(projectRoot)
 
 # SETUP PATHES
-rootScene = '{0}PROD/3D/scenes/'.format(rootProject)
-rootProd = '{0}PROD/'.format(rootProject)
+rootScene = '{0}PROD/3D/scenes/'.format(projectRoot)
+rootProd = '{0}PROD/'.format(projectRoot)
 charPath = 'ASSETS/CHARACTERS/'
 assetPath = 'ASSETS/PROPS/'
 envPath = 'ASSETS/ENVIRONMENTS/'
 mlPath = 'LOOKDEV/LIBRARY/MATERIALS/'
 llPath = 'LOOKDEV/LIBRARY/LIGHTS/'
-rootPB = '{0}PROD/RENDER_3D/BLAST/'.format(rootProject)
-rootABC = '{0}PROD/3D/cache/alembic/'.format(rootProject)
+rootPB = '{0}PROD/RENDER_3D/BLAST/'.format(projectRoot)
+rootABC = '{0}PROD/3D/cache/alembic/'.format(projectRoot)
+rootDic = '{}PREP/PIPELINE/DNA/DIC/'.format(projectRoot)
 
 # SETUP LISTS
 listAttr = ['mColor','mDisp','mMat']
 listShaders = [ 'aiStandard' , 'aiSkin' , 'aiHair' , 'aiUtility' , 'alSurface', 'jf_nested_dielectric', 'K_SSS', 'alHair', 'aiRaySwitch'] 
-# ASSEMBLER PHASES LISTS
+# ASSEMBLER LISTS
 listANM = ['RIG' , 'ANIMATION/' , 'ANM_' ]
 listRND = ['GEO', 'RENDER/' , 'RND_' ]
-
-# Setup FTRACK PROJECT
-codeProject = projectName.lower() # Get project name in lowercase
-project = ftrack.getProject( '{}'.format(codeProject) ) # Get project data from FTrack
